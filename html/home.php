e5ee3617be1e81e2f7c279d596d1822b8f214d08
tiveritz <lukas.walch@icloud.com>
2021-03-10 19:49:21 +0100

    [Feature] Landing page with functionality

--- html/home.php
+++ html/home.php
@@ -1,2 +1,42 @@
-<h1>home</h1>
-<p>this view is visible without authentication</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./html/style/home.css">
+    <title>Home</title>
+</head>
+<body id="home">
+    <div id="header">
+        <a href="mailto:lukas.walch@icloud.com" id="header-mail">
+            <div class="icon"></div>
+        </a>
+        <a href="https://github.com/tiveritz" target="_blank" id="header-github">
+            <div class="icon"></div>
+        </a>
+        <a href="https://www.linkedin.com/in/lukas-walch-5559ba1b6/" target="_blank" id="header-linkedin">
+            <div class="icon"></div>
+        </a>
+        <div class="header-empty">
+        </div>
+    </div>
+    <div id="info">
+        <div>
+            <div>At Some Point You'll Always</div>
+            <div>Have to testyurstuff.online</div>
+            <div class="size-mini">tiveritz</div>
+        </div>
+    </div>
+    <div id="nuts">
+        <a href="<?php echo ROOT.'/dashboard' ?>" id="nuts-link">
+            <div id="nuts-text">
+                <div>log in</div>
+                <div>and go</div>
+                <div>nuts</div>
+            </div>
+            <div id="play"></div>
+        </a>
+    </div>
+</body>
+</html>
